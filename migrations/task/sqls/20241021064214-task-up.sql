fc61d7afadd859913e46fb6e947261b0e9f6d09e
Rosa <ss91810@gmail.com>
2024-12-08 17:08:17 +0900

    1208

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -321,7 +321,7 @@ select
  sum (price_paid) as "總營收"
 from "CREDIT_PURCHASE" 
 where "CREDIT_PURCHASE".purchase_at >= '2024-11-01 00:00:00'
-and "CREDIT_PURCHASE".purchase_at <= '2024-11-30 23:59:59'
+and "CREDIT_PURCHASE".purchase_at <= '2024-11-30 23:59:59';
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
